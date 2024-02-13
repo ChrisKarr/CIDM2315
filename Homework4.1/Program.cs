485172934ff0d7063321f056ac93fe03c1d59a51
ChrisKarr <157043649+ChrisKarr@users.noreply.github.com>
2024-02-12 20:30:13 -0600

    Add files via upload

---
+++ Homework4.1/Program.cs
@@ -0,0 +1,25 @@
+﻿    using System;
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the first number:");
+            int A = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter the second number:");
+            int B = Convert.ToInt32(Console.ReadLine());
+            int largest = LargestNumber(A, B);
+            Console.WriteLine($"The largest number is {largest}");
+        }
+        static int LargestNumber(int A, int B)
+        {
+            if(A > B)
+            {
+                return A;
+            }
+            else
+            {
+                return B;
+            }
+        }
+    }
---
+++ Homework4.2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Reflection.Metadata;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Please enter a number:");
+        int num = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Now type (L) for left or (R) for right:");
+        string facing = Console.ReadLine();
+        Console.WriteLine($"Number = {num}, Shape = {facing}");
+
+        if(facing == "L")
+        {
+            FacingLeft(num);
+        }
+        else if(facing == "R")
+        {
+            FacingRight(num);
+        }
+    }
+
+    static void FacingLeft(int num)
+    {
+        for(int row = 0; row<num; row++)
+        {
+            for(int col = 0; col<=row; col++)
+            {
+                Console.Write('*');
+            }
+            Console.WriteLine();
+        }
+    }
+
+    static void FacingRight(int num)
+    {
+        for(int row = 0; row<num; row++)
+        {
+            for(int col = 0; col<num; col++)
+            {
+                if(col<num - row - 1)
+                Console.Write(' ');
+                else
+                Console.Write('*');
+            }
+            Console.WriteLine();
+        }
+    }
+}
